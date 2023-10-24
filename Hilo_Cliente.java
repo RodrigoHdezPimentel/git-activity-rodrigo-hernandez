3fc6e22948b5683fc81cf6155a6df8aee1afff8c
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2023-10-24 09:25:17 +0200

    Cliente done

---
+++ Hilo_Cliente.java
@@ -0,0 +1,15 @@
+package Practica_Cliente;
+
+public class Hilo_Cliente extends Thread{
+	private Cuenta_Bancaria cuenta;
+	
+	
+	public Hilo_Cliente(Cuenta_Bancaria cuenta) {
+		this.cuenta = cuenta;
+	}
+	
+	
+	public void run() {
+		cuenta.Ingresar(100);
+	}
+}
