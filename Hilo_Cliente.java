d305abf9250b1cb1a1bbfeb4d5ff051e913c0c60
Rodrigo <rodrigo.hdez.oficial@gmail.com>
2023-10-24 09:36:19 +0200

    Cliente actualizado para la cuenta bancaria

--- Hilo_Cliente.java
+++ Hilo_Cliente.java
@@ -2,7 +2,11 @@ package Practica_Cliente;
 
 public class Hilo_Cliente extends Thread{
 	private Cuenta_Bancaria cuenta;
-	
+
+	public Hilo_Cliente(Cuenta_Bancaria cuenta) {
+       		this.cuenta = cuenta;
+        }
+
 	public void run() {
 		cuenta.Ingresar(100);
 	}
